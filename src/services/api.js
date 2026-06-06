37da8ebef4d2a406dfa3e3465066768f05996026
devansh-kandhari-01 <devanshkandhari01@gmail.com>
2026-06-07 02:40:57 +0530

    Fourth commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = import.meta.env.API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 export async function getPopularMovies(){
     let response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`);
